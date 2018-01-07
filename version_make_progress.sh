17e32837ae5191751543c67540ba65a858686084
T-Max <tmax.tver.95@gmail.com>
2018-01-07 19:20:51 +0300

    Deleted some files

--- version_make_progress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# increments the real number held in version.h by 0.01
-
-read line < src_mod/versioning/version_build.h
-tokens=($line)
-version=${tokens[2]}
-newversion=$(echo $version + 1 | bc)
-echo "#define BUILD_NUMBER $newversion" > src_mod/versioning/version_build.h
